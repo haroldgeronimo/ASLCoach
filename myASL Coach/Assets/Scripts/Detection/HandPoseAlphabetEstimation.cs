8a37daaac11967e22018896b371b4dcab4abe4b7
AybanGee <ivangeronimo563@gmail.com>
2019-03-18 08:53:33 +0800

    update somethings

--- myASL Coach/Assets/Scripts/Detection/HandPoseAlphabetEstimation.cs
+++ myASL Coach/Assets/Scripts/Detection/HandPoseAlphabetEstimation.cs
@@ -23,6 +23,7 @@ namespace OpenCVModule {
         /// </summary>
         public FingerCountLibrary FCL;
         public HandPoseDetector HPD;
+
         /// <summary>
         /// The texture.
         /// </summary>
@@ -77,7 +78,6 @@ namespace OpenCVModule {
         /// The number of fingers.
         /// </summary>
         int numberOfFingers = 0;
-
 
         /// <summary>
         /// The web cam texture to mat helper.
@@ -207,7 +207,8 @@ namespace OpenCVModule {
                     storedTouchPoint = null;
                 }
 
-                handPoseEstimationProcess (rgbaMat);
+                handPoseEstimationProcessNew (rgbaMat);
+                //handPoseEstimationProcess (rgbaMat);
 
                 //Imgproc.putText (rgbaMat, "Please touch the area of the open hand.", new Point (5, rgbaMat.rows () - 10), Core.FONT_HERSHEY_SIMPLEX, 1.0, new Scalar (255, 255, 255, 255), 2, Imgproc.LINE_AA, false);
 
@@ -223,7 +224,7 @@ namespace OpenCVModule {
         void OnDisable () {
             webCamTextureToMatHelper.Dispose ();
         }
-    
+
         /// <summary>
         /// Raises the play button event.
         /// </summary>
@@ -360,7 +361,7 @@ namespace OpenCVModule {
             //                      Debug.Log ("numberOfFingers " + numberOfFingers);
 
             //                      Core.putText (mRgba, "" + numberOfFingers, new Point (mRgba.cols () / 2, mRgba.rows () / 2), Core.FONT_HERSHEY_PLAIN, 4.0, new Scalar (255, 255, 255, 255), 6, Core.LINE_AA, false);
-           // numberOfFingersText.text = letter;
+            // numberOfFingersText.text = letter;
 
             foreach (Point p in listPoDefect) {
                 Imgproc.circle (rgbaMat, p, 6, new Scalar (255, 0, 255, 255), -1);
@@ -530,6 +531,272 @@ namespace OpenCVModule {
             //                      Debug.Log ("dstPointMat " + dstPointMat.dump ());
 
             return dstPointMat.toArray () [0];
+        }
+        double lower_thresh1 = 129;
+        double upper_thresh1 = 255;
+        public void handPoseEstimationProcessNew (Mat rgbaMat) {
+            Mat gray = new Mat ();
+            Imgproc.cvtColor (rgbaMat, gray, Imgproc.COLOR_RGB2GRAY);
+
+            Mat hsv = new Mat ();
+            Imgproc.cvtColor (rgbaMat, hsv, Imgproc.COLOR_RGB2HSV);
+
+            /*     int[] lower_red = new int[]{0,150,50};
+            int[] upper_red = new int[]{195,255,255};
+ */
+
+            Mat blurred = new Mat ();
+            Imgproc.GaussianBlur (gray, blurred, new OpenCVForUnity.Size (35, 35), 1, 1);
+            detector.process (rgbaMat);
+            Mat tresh1 = new Mat ();
+            Imgproc.threshold (blurred, tresh1, lower_thresh1, upper_thresh1, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
+
+            Mat tresh_dofh = new Mat ();
+            Imgproc.threshold (rgbaMat, tresh_dofh, lower_thresh1, upper_thresh1, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
+            /* if (!isColorSelected)
+                return; */
+
+            List<MatOfPoint> contours = new List<MatOfPoint> ();
+            Mat srcHierarchy = new Mat ();
+            Imgproc.findContours (tresh1, contours, srcHierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+            List<MatOfPoint> xyz, hierarchy = new List<MatOfPoint> (contours);
+
+            List<MatOfPoint> contours_dofh = new List<MatOfPoint> ();
+            Mat srcHierarchy_dofh = new Mat ();
+            Imgproc.findContours (tresh_dofh, contours_dofh, srcHierarchy_dofh, 2, 1);
+            List<MatOfPoint> xyz_dofh, hierarchy_dofh = new List<MatOfPoint> (contours_dofh);
+            MatOfPoint cnt = new MatOfPoint ();
+            foreach (MatOfPoint contour in contours) {
+                double _cnt = 0;
+                double currentContour = Imgproc.contourArea (contour);
+                if (currentContour > _cnt) {
+                    cnt = contour;
+                }
+            }
+            double area_of_contour = Imgproc.contourArea (cnt);
+            OpenCVForUnity.Rect rect = new OpenCVForUnity.Rect ();
+            double x, y, w, h;
+
+            rect = Imgproc.boundingRect (cnt);
+            x = rect.x;
+            y = rect.y;
+            w = rect.width;
+            h = rect.height;
+            Imgproc.rectangle (rgbaMat, new Point (x, y), new Point (x + w, y + h), new Scalar (0, 0, 255));
+            MatOfInt hull = new MatOfInt ();
+            Imgproc.convexHull (cnt, hull);
+
+            Imgproc.drawContours (rgbaMat, new List<MatOfPoint> () { cnt }, 0, new Scalar (0, 255, 0), 0);
+
+            Imgproc.convexHull (cnt, hull, false);
+            MatOfInt4 defects = new MatOfInt4 ();
+            Imgproc.convexityDefects (cnt, hull, defects);
+
+            int count_defects = 0;
+            Imgproc.drawContours (tresh1, contours, -1, new Scalar (0, 255, 0), 3);
+
+            List<Point> cntList = cnt.toList ();
+        
+            List<int> defectsList = defects.toList ();
+            for (int i = 0; i < defectsList.Count; i++) {
+                int s, e, f, d;
+                s = e = f = d = defectsList[i];
+                Vector2 start = new Vector2 ((float) cntList[s].x, (float) cntList[s].y);
+                Vector2 end = new Vector2 ((float) cntList[e].x, (float) cntList[e].y);
+                Vector2 far = new Vector2 ((float) cntList[f].x, (float) cntList[f].y);
+
+                float a = Mathf.Sqrt (Mathf.Pow ((start.x - start.x), 2) + Mathf.Pow ((start.y - start.y), 2));
+                float b = Mathf.Sqrt (Mathf.Pow ((far.x - start.x), 2) + Mathf.Pow ((far.y - start.y), 2));
+                float c = Mathf.Sqrt (Mathf.Pow ((end.x - far.x), 2) + Mathf.Pow ((end.y - far.y), 2));
+
+                float defect_angle = Mathf.Acos ((Mathf.Pow (b, 2) + Mathf.Pow (c, 2) - Mathf.Pow (a, 2)) / (2 * b * c)) * 60;
+                Imgproc.circle (rgbaMat, new Point ((double) far.x, (double) far.y), 4, new Scalar (0, 0, 255), -1);
+
+                if (defect_angle <= 90)
+                    count_defects += 1;
+
+                Imgproc.line (rgbaMat, new Point ((double) start.x, (double) start.y), new Point ((double) end.x, (double) end.y), new Scalar (0, 255, 0), 3);
+
+            }
+
+            Moments moment = Imgproc.moments (cnt);
+            MatOfPoint2f cnt2f = new MatOfPoint2f (cnt);
+            double perimeter = Imgproc.arcLength (cnt2f, true);
+            double area = Imgproc.contourArea (cnt);
+
+            Point center = new Point ();
+            float[] rad = new float[1] { 0 };
+            Imgproc.minEnclosingCircle (cnt2f, center, rad);
+            int radius = (int) rad[0];
+            Imgproc.circle (rgbaMat, center, radius, new Scalar (255, 0, 0), 2);
+            float area_of_circle = Mathf.PI * radius * radius;
+
+            MatOfInt hull_test = new MatOfInt ();
+            Imgproc.convexHull (cnt, hull_test);
+            double hull_area = Imgproc.contourArea (hull_test);
+            float solidity = (float) area / (float) hull_area;
+
+            double aspect_ratio = w / h;
+            double rect_area = w * h;
+            double extent = area / rect_area;
+            RotatedRect angle_t = Imgproc.fitEllipse (cnt2f);
+            double angle = angle_t.angle;
+
+            if (area_of_circle - area < 5000) {
+                letter = "A";
+                // numberOfFingersText.text = letter;
+            } else if (count_defects == 1) {
+                if (angle < 10)
+                    letter = "V";
+                else if (angle > 40 && angle < 66)
+                    letter = "C";
+                else if (angle > 20 && angle < 35)
+                    letter = "L";
+                else
+                    letter = "Y";
+            } else if (count_defects == 2) {
+                if (angle > 100)
+                    letter = "F";
+                else
+                    letter = "W";
+            } else if (count_defects == 4) {
+                letter = "A";
+            } else {
+                if (area > 12000)
+                    letter = "B";
+                else {
+                    if (solidity < 0.85) {
+                        if (aspect_ratio < 1) {
+                            if (angle < 20)
+                                letter = "D";
+                            else if (angle < 180 && angle > 169)
+                                letter = "I";
+                            else if (angle < 168)
+                                letter = "J";
+                        } else if (aspect_ratio > 1.01)
+                            letter = "Y";
+                    } else {
+                        if (angle > 30 && angle < 100)
+                            letter = "H";
+                        else if (angle > 120)
+                            letter = "I";
+                        else
+                            letter = "U";
+                    }
+                }
+            }
+
+            string status = "";
+            status += "\nRadius:" + (Mathf.PI * radius * radius).ToString () + " Area:" + area;
+            status += "\neffective circle area:" + (area_of_circle - area).ToString ();
+            status += "\nsolidity:" + solidity;
+            status += "\naspect ratio:" + aspect_ratio;
+            status += "\ncovexity defects:" + count_defects;
+            status += "\nextent:" + extent;
+            status += "\nangle:" + angle;
+            status += "\n Letter:" + letter;
+
+            HPD.DebugClear();
+            HPD.DebugWrite(status);
+            /* 
+                        // Imgproc.drawContours(rgbaMat,new List<MatOfInt>(){hull},0,new Scalar(0,255,255),0);
+                        detector.process (rgbaMat);
+
+                        //                      Debug.Log ("Contours count: " + contours.Count);
+
+                        RotatedRect rect = Imgproc.minAreaRect (new MatOfPoint2f (contours[0].toArray ()));
+
+                        double boundWidth = rect.size.width;
+                        double boundHeight = rect.size.height;
+                        int boundPos = 0;
+
+                        for (int i = 1; i < contours.Count; i++) {
+                            rect = Imgproc.minAreaRect (new MatOfPoint2f (contours[i].toArray ()));
+                            if (rect.size.width * rect.size.height > boundWidth * boundHeight) {
+                                boundWidth = rect.size.width;
+                                boundHeight = rect.size.height;
+                                boundPos = i;
+                            }
+                        }
+
+                        OpenCVForUnity.Rect boundRect = Imgproc.boundingRect (new MatOfPoint (contours[boundPos].toArray ()));
+                        Imgproc.rectangle (rgbaMat, boundRect.tl (), boundRect.br (), CONTOUR_COLOR_WHITE, 2, 8, 0);
+
+                        //                      Debug.Log (
+                        //                      " Row start [" + 
+                        //                              (int)boundRect.tl ().y + "] row end [" +
+                        //                              (int)boundRect.br ().y + "] Col start [" +
+                        //                              (int)boundRect.tl ().x + "] Col end [" +
+                        //                              (int)boundRect.br ().x + "]");
+
+                        double a = boundRect.br ().y - boundRect.tl ().y;
+                        a = a * 0.7;
+                        a = boundRect.tl ().y + a;
+
+                        //                      Debug.Log (
+                        //                      " A [" + a + "] br y - tl y = [" + (boundRect.br ().y - boundRect.tl ().y) + "]");
+
+                        //Core.rectangle( mRgba, boundRect.tl(), boundRect.br(), CONTOUR_COLOR, 2, 8, 0 );
+                        Imgproc.rectangle (rgbaMat, boundRect.tl (), new Point (boundRect.br ().x, a), CONTOUR_COLOR, 2, 8, 0);
+
+                        MatOfPoint2f pointMat = new MatOfPoint2f ();
+                        Imgproc.approxPolyDP (new MatOfPoint2f (contours[boundPos].toArray ()), pointMat, 3, true);
+                        contours[boundPos] = new MatOfPoint (pointMat.toArray ());
+
+                        //MatOfInt hull = new MatOfInt ();
+                        MatOfInt4 convexDefect = new MatOfInt4 ();
+                        Imgproc.convexHull (new MatOfPoint (contours[boundPos].toArray ()), hull);
+
+                        if (hull.toArray ().Length < 3)
+                            return;
+
+                        Imgproc.convexityDefects (new MatOfPoint (contours[boundPos].toArray ()), hull, convexDefect);
+
+                        List<MatOfPoint> hullPoints = new List<MatOfPoint> ();
+                        List<Point> listPo = new List<Point> ();
+                        for (int j = 0; j < hull.toList ().Count; j++) {
+                            listPo.Add (contours[boundPos].toList () [hull.toList () [j]]);
+                        }
+
+                        MatOfPoint e = new MatOfPoint ();
+                        e.fromList (listPo);
+                        hullPoints.Add (e);
+
+                        List<MatOfPoint> defectPoints = new List<MatOfPoint> ();
+                        List<Point> listPoDefect = new List<Point> ();
+                        for (int j = 0; j < convexDefect.toList ().Count; j = j + 4) {
+                            Point farPoint = contours[boundPos].toList () [convexDefect.toList () [j + 2]];
+                            int depth = convexDefect.toList () [j + 3];
+                            if (depth > threasholdSlider.value && farPoint.y < a) {
+                                listPoDefect.Add (contours[boundPos].toList () [convexDefect.toList () [j + 2]]);
+                            }
+                            //                              Debug.Log ("defects [" + j + "] " + convexDefect.toList () [j + 3]);
+                        }
+
+                        MatOfPoint e2 = new MatOfPoint ();
+                        e2.fromList (listPo);
+                        defectPoints.Add (e2);
+
+                        //                      Debug.Log ("hull: " + hull.toList ());
+                        //                      Debug.Log ("defects: " + convexDefect.toList ());
+
+                        Imgproc.drawContours (rgbaMat, hullPoints, -1, CONTOUR_COLOR, 3);
+
+                        //                      int defectsTotal = (int)convexDefect.total();
+                        //                      Debug.Log ("Defect total " + defectsTotal);
+
+                        this.numberOfFingers = listPoDefect.Count;
+                        if (this.numberOfFingers > 5)
+                            this.numberOfFingers = 5;
+                        letter = FCL.GuessLetter (numberOfFingers).ToString ();
+                        //                      Debug.Log ("numberOfFingers " + numberOfFingers);
+
+                        //                      Core.putText (mRgba, "" + numberOfFingers, new Point (mRgba.cols () / 2, mRgba.rows () / 2), Core.FONT_HERSHEY_PLAIN, 4.0, new Scalar (255, 255, 255, 255), 6, Core.LINE_AA, false);
+                        // numberOfFingersText.text = letter;
+
+                        foreach (Point p in listPoDefect) {
+                            Imgproc.circle (rgbaMat, p, 6, new Scalar (255, 0, 255, 255), -1);
+                        } */
         }
     }
 }
